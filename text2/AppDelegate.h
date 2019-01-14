81004732a7361b4cdf22bd45785aed8fe63e4f0f
ssq <ssqsunqin@163.com>
2019-01-14 16:28:21 +0800

    Initial Commit

---
+++ text2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  text2
+//
+//  Created by topwellsoft on 2019/1/14.
+//  Copyright © 2019年 ssq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ text2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  text2
+//
+//  Created by topwellsoft on 2019/1/14.
+//  Copyright © 2019年 ssq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
